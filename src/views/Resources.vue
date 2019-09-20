8d68bdbf9b60b30be396f9b2c18c4e1acdeb04d2
CheskoTECH <cheskidov.peter@yandex.ru>
2019-09-20 21:49:33 +0500

    Added graphics. Not working

--- src/views/Resources.vue
+++ src/views/Resources.vue
@@ -2,7 +2,8 @@
   <div class="main">
     <div class="main__firstRow">
       <div class="main__firstRow--firstCol common">
-
+        <v-gauge :value="processor" unit="%"></v-gauge>
+        <v-gauge :value="memory" unit="%"></v-gauge>
       </div>
       <div class="main__firstRow--secondCol">
         <div class="main__firstRow--secondCol-topBlock common">
@@ -20,13 +21,56 @@
 </template>
 
 <script>
-export default {
+import VGauge from "vgauge";
 
-}
+export default {
+  components: {
+    VGauge
+  },
+  data() {
+    return {
+      processor: processorFunc(),
+      memory: 0,
+      data: 0
+    };
+  },
+  mounted() {
+    // if (this.processor < 100) {
+    //   //setInterval(() => {
+    //     this.processor += 2
+    //   //}, 200);
+    // }
+
+    // if (this.processor >= 100) {
+    //   this.processor -= 20;
+    // }
+
+    setInterval(() => {
+      if (this.memory < 100) this.memory += 5;
+    }, 100)
+  },
+  methods: {
+    processorFunc() {
+      let ldata = this.data;
+      if (ldata < 100) {
+        //setInterval(() => {
+          ldata += 2
+        //}, 200);
+      }
+
+      if (ldata >= 100) {
+        ldata -= 20;
+      }
+      return ldata;
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-
+.gauge-title {
+  display: none;
+}
 .main {
   height: 100%;
   display: grid;
@@ -42,6 +86,10 @@ export default {
 
     &--firstCol {
       background-color: #fff;
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      align-items: center;
+      justify-items: center;
     }
 
     &--secondCol {
